6198c4c5294afcd50e5012716dda13ac4fdc02f0
Yehui Zhang <yehuizhang@outlook.com>
2022-01-01 00:02:23 -0800

    currency-exchange-service init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(1, 'USD', 'EUR', 1.5, '');
+
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(2, 'USD', 'CNY', 6.8, '');
+
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(3, 'USD', 'JPY', 101.2, '');
