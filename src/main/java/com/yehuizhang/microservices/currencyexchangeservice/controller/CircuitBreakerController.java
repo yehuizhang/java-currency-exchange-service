8c2f9e4b766ae79895f7dd6efc6db55cba04315d
Yehui Zhang <yehuizhang@outlook.com>
2022-01-01 23:40:05 -0800

    circuit breaker: Retry

---
+++ src/main/java/com/yehuizhang/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,29 @@
+package com.yehuizhang.microservices.currencyexchangeservice.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private final Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api", fallbackMethod = "fallbackMethod")
+    public String sampleApi() {
+
+        logger.info("Sample API call received");
+        ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("http://localhost:7777/dummy", String.class);
+
+        return responseEntity.getBody();
+    }
+
+    public String fallbackMethod(Exception ex) {
+        return "fallback response";
+    }
+}
--- src/main/java/com/yehuizhang/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/yehuizhang/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,12 +1,12 @@
-package com.yehuizhang.microservices.currencyexchangeservice;
+package com.yehuizhang.microservices.currencyexchangeservice.controller;
 
+import com.yehuizhang.microservices.currencyexchangeservice.CurrencyExchange;
+import com.yehuizhang.microservices.currencyexchangeservice.CurrencyExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
@@ -20,7 +20,7 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
-        if(currencyExchange == null) {
+        if (currencyExchange == null) {
             throw new RuntimeException("Unable to find data for " + from + " to " + to);
         }
         String port = environment.getProperty("local.server.port");
