b2d3e264045d99ba8a0e295f0a7fa3b9e388b20d
Yehui Zhang <yehuizhang@outlook.com>
2022-01-03 23:02:50 -0800

    Distributed Tracing: Zipkin

--- src/main/java/com/yehuizhang/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/yehuizhang/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.yehuizhang.microservices.currencyexchangeservice.controller;
 
 import com.yehuizhang.microservices.currencyexchangeservice.CurrencyExchange;
 import com.yehuizhang.microservices.currencyexchangeservice.CurrencyExchangeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+    private final Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private CurrencyExchangeRepository currencyExchangeRepository;
 
@@ -19,6 +23,8 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+
+        logger.info("retrieveExchangeValue called with {} and {}", from, to);
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
         if (currencyExchange == null) {
             throw new RuntimeException("Unable to find data for " + from + " to " + to);
